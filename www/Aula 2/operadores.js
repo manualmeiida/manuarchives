a3426b83f1cf2c17d98105c64df841cb20ccbbf2
manu <emanuelealmeiida@icloud.com>
2025-04-09 16:51:07 -0300

    Add files via upload

---
+++ www/Aula 2/operadores.js
@@ -0,0 +1,23 @@
+//igualdade de valor (==)
+console.log(5 == 5)
+
+//diferença de valor (!=)
+console.log(5 != 5)
+
+//maior que (>)
+console.log(10 > 7)
+
+//menor que (<)
+console.log(3 < 5)
+
+//maior ou igual a (>=)
+console.log(7 >= 7)
+
+//menor ou igual a (<=)
+console.log(4 <= 9)
+
+//E (&&)
+console.log(5 == 5 && 10 > 7)
+
+//OU (||)
+console.log(5 == 5 || 5 > 7)
