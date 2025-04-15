5101f6f1f3a1d5995bb53c54dd539c58e2720c42
manu <emanuelealmeiida@icloud.com>
2025-04-15 15:43:16 -0300

    Delete www/funcoes_01.js

--- www/funcoes_01.js
+++
@@ -1,8 +0,0 @@
-//função que exibe uma saudaçãi no console
-function mostrarSaudacao() {
-    var mensagem = "Olá! Seja bem-vindo ao mundo da programação";
-    console.log(mensagem);
-}
-
-//chamando a função
-mostrarSaudacao();
