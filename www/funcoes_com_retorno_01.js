5e4ea0ae41dff16ad18d877169d01b463dc0c8f6
manu <emanuelealmeiida@icloud.com>
2025-04-15 15:43:46 -0300

    Delete www/funcoes_com_retorno_01.js

--- www/funcoes_com_retorno_01.js
+++
@@ -1,9 +0,0 @@
-//função que retorna o dobo de um numero
-function dobro(numero) {
-    var resultado = numero * 2
-    return resultado;
-}
-
-//usando a função e mostrando o valor retornado
-var valor = dobro(5);
-console.log("O dobro de 5 é: " + valor);
