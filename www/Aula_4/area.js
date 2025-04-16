3b55ffd4610bea2890c5620946bbeed1eb5765ee
manu <emanuelealmeiida@icloud.com>
2025-04-16 13:10:38 -0300

    Add files via upload

---
+++ www/Aula_4/area.js
@@ -0,0 +1,6 @@
+function calcularArea (base, altura) {
+    var resultado = base * altura
+    console.log("A area do triângulo é " + resultado + " m²");
+}
+
+calcularArea (5, 6)
---
+++ www/Aula_4/endereco.js
@@ -0,0 +1,5 @@
+function endereco(rua,numero,bairro,cidade,estado,cep) {
+    console.log(rua + "," + numero + "," + bairro + "," + cidade + "-" + estado + "," + cep )
+}
+
+endereco("Rua", 100, "Centro", "Valença", "RJ", "27600-000")
